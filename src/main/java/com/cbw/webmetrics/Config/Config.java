73a348e9a8232c09a05ea2eb40f05279d6fc436b
alwaysbeloved <scu_cbw@qq.com>
2018-12-13 22:14:48 +0800

    hide config

--- src/main/java/com/cbw/webmetrics/Config/Config.java
+++ src/main/java/com/cbw/webmetrics/Config/Config.java
@@ -8,18 +8,18 @@ public class Config {
     /**
      * email config
      */
-    public static final String MY_EMAIL_ACCOUNT = "webmetrics@163.com";
-    public static final String MY_EMAIL_PASSWORD = "Cangsang666";
-    public static final String MY_EMAIL_SMTP_HOST = "smtp.163.com";
-    public static final String SMTP_PORT = "465";
+    public static final String MY_EMAIL_ACCOUNT = "xxx";
+    public static final String MY_EMAIL_PASSWORD = "xxx";
+    public static final String MY_EMAIL_SMTP_HOST = "xxx";
+    public static final String SMTP_PORT = "xxx";
 
     /**
      * message prefix
      */
-    public static final String BASE_URL = "https://api.miaodiyun.com/20150822";
-    public static final String ACCOUNT_SID = "500c61ed128347a1805d09085e6cef18";
-    public static final String AUTH_TOKEN = "95336cee355b47dda31e3ba91a0eb4c7";
-    public static final String RESP_DATA_TYPE = "json";
+    public static final String BASE_URL = "xxx";
+    public static final String ACCOUNT_SID = "xxx";
+    public static final String AUTH_TOKEN = "xxx";
+    public static final String RESP_DATA_TYPE = "xxx";
     public static final String MESSAGE_PREFIX = "【WebWarning】尊敬的用户，您的验证码为";
     public static final String OPERATION = "/industrySMS/sendSMS";
 }
